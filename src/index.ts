5647d533cfcd2918dd1d01b0cddc2ff26929fc04
Eliot Fagnon <efagnon2002@gmail.com>
2023-04-10 23:33:35 +0100

    feat: init server on port 5050

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import Express, { Application } from "express";
+import cors from "cors";
+import logger from "./utils/logger";
 
 const app: Application = Express();
+
+app.use(cors);
+
+app.listen(5050, () => {
+  logger.info("App started on port 5050.")
+})
