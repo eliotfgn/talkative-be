c2b51982b64b4315da2db20570fcf02c1f69726d
Eliot Fagnon <efagnon2002@gmail.com>
2023-05-18 12:25:36 +0100

    feat: define users routes

---
+++ src/routers/users.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router: Router = Router();
+
+router.post("/");
+
+router.get("/");
+
+router.get("/:id");
+
+router.put("/:id");
+
+router.delete("/:id");
