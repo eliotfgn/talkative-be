fc4e5e926e9983b19c25c8a539fc79280be077a8
Eliot Fagnon <efagnon2002@gmail.com>
2023-05-23 14:12:29 +0100

    feat: create new user

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,9 +1,26 @@
 import { UserDto } from "./user.dto";
-import { User } from "@prisma/client";
+import { validate } from "class-validator";
+import { Prisma, User } from "@prisma/client";
+import userRepository from "../../repositories/user.repository";
 
 class UserService {
-  async create(user: UserDto): Promise<Partial<UserDto>> {
+  async create(payload: UserDto): Promise<User> {
+    validate(payload).then(error => { });
 
+    const user: Prisma.UserCreateInput = {
+      username: payload.username,
+      email: payload.email,
+      password: payload.password,
+      firstname: payload.firstname,
+      lastname: payload.lastname,
+      profilePic: payload.profilePic,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    };
+
+    return userRepository.create({
+      data: user
+    });
   }
 
   async findAll(): Promise<Partial<UserDto>[]> {
