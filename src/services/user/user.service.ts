65b604f17f6c492bdb438f3a9a04ced5cd233838
Eliot Fagnon <efagnon2002@gmail.com>
2023-05-26 04:22:48 +0100

    fix: remove unimplement method errors

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,64 +1,16 @@
-import { UserDto } from './user.dto';
-import { validate } from 'class-validator';
-import { Prisma, User } from '@prisma/client';
-import userRepository from '../../repositories/user.repository';
-
 class UserService {
-  async create(payload: UserDto): Promise<User> {
-    validate(payload).then((error) => {});
+  async create(): Promise<any> {}
 
-    const user: Prisma.UserCreateInput = {
-      username: payload.username,
-      email: payload.email,
-      password: payload.password,
-      firstname: payload.firstname,
-      lastname: payload.lastname,
-      profilePic: payload.profilePic,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
+  // @ts-ignore
+  async findAll(): Promise<any[]> {}
 
-    return userRepository.create({
-      data: user,
-    });
-  }
+  async findById(id: string): Promise<any> {}
 
-  async findAll(): Promise<any[]> {
-    let users: {
-      createdAt: Date;
-      firstname: string | null;
-      profilePic: string | null;
-      id: string;
-      email: string;
-      username: string;
-      lastname: string | null;
-      updatedAt: Date;
-    }[];
+  async findByEmail(email: string): Promise<any> {}
 
-    const data = await userRepository.findMany();
+  async update(id: string): Promise<any> {}
 
-    users = data.map((user) => {
-      return {
-        id: user.id,
-        username: user.username,
-        email: user.email,
-        firstname: user.firstname,
-        lastname: user.lastname,
-        profilePic: user.profilePic,
-        createdAt: user.createdAt,
-        updatedAt: user.updatedAt,
-      };
-    });
-    return users;
-  }
-
-  async findById(id: string): Promise<Partial<UserDto>> {}
-
-  async findByEmail(email: string): Promise<Partial<UserDto>> {}
-
-  async update(id: string, user: UserDto): Promise<Partial<UserDto>> {}
-
-  async delete(id: string): Promise<boolean> {}
+  async delete(id: string): Promise<any> {}
 }
 
 export default UserService;
