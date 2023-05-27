309c652bc60c79c3c594e85d3f94da0a014cc64b
Eliot Fagnon <efagnon2002@gmail.com>
2023-05-28 00:34:36 +0100

    feat: register user routes in app

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import Express, { Application, json } from 'express';
 import cors from 'cors';
 import logger from './utils/logger';
 import authRouter from './routers/auth';
+import userRouter from './routers/users';
 import { verifyToken } from './middlewares/auth';
 
 const app: Application = Express();
@@ -12,6 +13,8 @@ app.use(json());
 app.use('/auth', authRouter);
 
 app.use(verifyToken);
+
+app.use('/users', userRouter);
 
 app.listen(5050, () => {
   logger.info('App started on port 5050.');
