92050c6cb70ca484fbf05fc30acfbe437a444dbf
Eliot Fagnon <efagnon2002@gmail.com>
2023-05-27 07:28:54 +0100

    feat: register auth routes in the app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import Express, { Application } from "express";
-import cors from "cors";
-import logger from "./utils/logger";
+import Express, { Application, json } from 'express';
+import cors from 'cors';
+import logger from './utils/logger';
+import authRouter from './routers/auth';
 
 const app: Application = Express();
 
 app.use(cors);
+app.use(json());
+
+app.use('/auth', authRouter);
 
 app.listen(5050, () => {
-  logger.info("App started on port 5050.")
-})
+  logger.info('App started on port 5050.');
+});
--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import AuthService from '../services/auth/auth.service';
+import AuthController from '../controllers/auth.controller';
 
 const router: Router = Router();
-const authService: AuthService = new AuthService();
+const authController: AuthController = new AuthController();
 
-router.post('/register', authService.register);
-router.post('/login', authService.login);
+router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 export default router;
