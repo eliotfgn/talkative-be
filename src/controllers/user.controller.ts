db3d0bac96a52637b019cbfd0a062b98808d2a95
Eliot Fagnon <efagnon2002@gmail.com>
2023-05-28 00:30:58 +0100

    feat: getting connected user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,25 +1,51 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
+import UserService from '../services/user/user.service';
+import logger from '../utils/logger';
 
 class UserController {
-  async getAll(req: Request, res: Response) {
+  userService: UserService;
 
+  constructor() {
+    this.userService = new UserService();
   }
 
-  async getById(req: Request, res: Response) {
+  async getAll(req: Request, res: Response) {}
 
-  }
+  async getById(req: Request, res: Response) {}
 
-  async getConnectedUser(req: Request, res: Response) {
+  getConnectedUser = async (req: Request, res: Response) => {
+    // @ts-ignore
+    const userId: string = req.user;
+    let userResponse;
 
-  }
+    try {
+      userResponse = await this.userService.findById(userId);
 
-  async update(req: Request, res: Response) {
+      if (userResponse) {
+        res.status(200).json({
+          success: true,
+          message: 'User successfully retrieved.',
+          data: userResponse,
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: 'User not found.',
+        });
+      }
+    } catch (error) {
+      const e = error as Error;
+      res.status(500).json({
+        success: false,
+        message: 'An unexpected error occurs.',
+      });
+      logger.error(e.message);
+    }
+  };
 
-  }
+  async update(req: Request, res: Response) {}
 
-  async remove(req: Request, res: Response) {
-
-  }
+  async remove(req: Request, res: Response) {}
 }
 
 export default UserController;
