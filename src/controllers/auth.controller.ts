0da302f2235c8cb3ef5df0a3e5c31cb69c9f0e49
Eliot Fagnon <efagnon2002@gmail.com>
2023-05-27 04:17:59 +0100

    feat: handle user login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,8 +1,9 @@
 import AuthService from '../services/auth/auth.service';
 import { Request, Response } from 'express';
 import { User, UserResponse } from '../types/user';
-import { EmailExistsError, UsernameExistsError } from '../errors/user.error';
+import { EmailExistsError, UsernameExistsError, UserNotFoundError } from '../errors/user.error';
 import { ErrorResponse } from '../types/error';
+import { IncorrectPasswordError } from '../errors/auth.error';
 
 class AuthController {
   authService: AuthService;
@@ -36,6 +37,37 @@ class AuthController {
     }
 
     response.status(201).json(userResponse);
+  }
+
+  async login(request: Request, response: Response) {
+    const payload: { email: string; password: string } = request.body;
+    let token: string;
+
+    try {
+      token = await this.authService.login(payload);
+
+      response.status(200).json({
+        success: true,
+        message: 'Successfully authenticated.',
+        data: {
+          token: token,
+        },
+      });
+    } catch (error) {
+      if (error instanceof IncorrectPasswordError) {
+        response.status(403).json({
+          success: false,
+          status: 403,
+          message: 'Incorrect password.',
+        });
+      } else if (error instanceof UserNotFoundError) {
+        response.status(404).json({
+          success: false,
+          status: 404,
+          message: 'User not found.',
+        });
+      }
+    }
   }
 }
 
