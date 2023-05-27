0e0960014bb7a2acb02f56a5beccb698e6e7c318
Eliot Fagnon <efagnon2002@gmail.com>
2023-05-27 03:46:26 +0100

    feat: user login

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import UserService from '../user/user.service';
 import { User, UserResponse } from '../../types/user';
+import { Account } from '@prisma/client';
+import { compare } from '../../utils/password.util';
+import { generateAccessToken } from '../../utils/jwt.util';
 
 class AuthService {
   userService: UserService;
@@ -10,5 +13,17 @@ class AuthService {
 
   async register(payload: User): Promise<UserResponse> {
     return await this.userService.create(payload);
+  }
+
+  async login(payload: { email: string; password: string }) {
+    const account: Account | null = await this.userService.findByEmail(payload.email);
+
+    if (!account) return null;
+
+    if (await compare(payload.password, account.password)) {
+      return generateAccessToken(account.id);
+    } else {
+      return null;
+    }
   }
 }
