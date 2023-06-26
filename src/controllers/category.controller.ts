fe33601580a194f544c845fa7935d7512d77034a
Eliot Fagnon <efagnon2002@gmail.com>
2023-06-26 12:16:58 +0100

    feat: add controller for category creation

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,3 +1,28 @@
+import { Category } from '@prisma/client';
+import { Request, Response } from "express";
+import CategoryService from "../services/category/category.service";
+import { CategoryDtoType } from "../services/category/category.dto";
+import logger from '../utils/logger';
+
 class CategoryController {
-    
+    private categoryService: CategoryService;
+
+    constructor() {
+        this.categoryService = new CategoryService();
+    }
+
+    create = async (request: Request, response: Response) => {
+        const categoryPayload: CategoryDtoType = request.body;
+        try {
+            const category: Category = await this.categoryService.create(categoryPayload);
+            
+            response.status(201).json({
+                success: true,
+                message: 'Category successfully created',
+                data: category
+            });
+        } catch (error: any) {
+            logger.error(error.message);
+        }
+    }
 }
